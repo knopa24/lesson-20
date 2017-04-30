1af01938025fef2548ef9636428ea6cb2f2e9d86
knopa24 <alina.lwow@gmail.com>
2017-04-30 14:54:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var x;
+
+x = 5;
+
+var y = 10;
+
+var resultAdd = x + y;
+console.log('resultAdd');
+
+var resultSub = x - y;
+console.log('resultSub');
+
+var resultMult=x*y;
+
+var resultDiv=x/y
+console.log('Додавання:', resultAdd);
+console.log('Віднімання:', resultSub);
+console.log('Множення:', resultMult);
+console.log('Ділення:', resultDiv);
+
+var firstNumber =Number (prompt('Enter first number'));
+var secondNumber =Number (prompt('Enter second number'));
+
+console.log('First number:',firstNumber);
+console.log('Second number:',secondNumber);
+
+var Add = firstNumber + secondNumber;
+var Sub = firstNumber - secondNumber;
+var Mult = firstNumber * secondNumber;
+var Div = firstNumber / secondNumber;
+var Mod = firstNumber % secondNumber;
+var Inc = ++firstNumber;
+var Dec = --secondNumber;
+
+console.log('Add',Add);
+console.log('Sub',Sub);
+console.log('Mult',Mult);
+console.log('Div',Div);
+console.log('mod',Mod);
+console.log('inc',Inc);
+console.log('dec',Dec);
+
+var txt1 ='My first text';
+var txt2 ='My second text';
+
+document.write(txt1+txt2);
+
+
+
+
+var usertxt =prompt ('Your text');
+document.write(usertxt);
